518040bb76bbf80edbbdee091115174d0c4f217b
CalvinA-C <calvinalex.currie@gmail.com>
2024-09-18 20:37:12 +1000

    mountify--rootforDomain-not-subDirectory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Mountify/", // Ensure this matches your repository name for GitHub Pages
+  base: "/", // This should be the base path for your site
 });
