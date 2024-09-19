fd86b2e041097fd3479143f4f16a9af75b7b1b01
CalvinA-C <calvinalex.currie@gmail.com>
2024-09-19 10:51:20 +1000

    mountify--imagingIssues

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import "./Reviews.css";
 import reviews from "../constrants/reviews";
-import backGroundImage from "../assets/blvck-paris-ckBxIZmpXJE-unsplash.jpg";
+import backGroundImage from "../assets/reviewsBackground-image.jpg";
 
 const Reviews = () => {
   return (
     <div className="mountify__reviews section__marginTop">
-      <img
-        className="mountify__review--backgroundImage"
-        src={backGroundImage}
-        alt=""
-      />
+      <div className="mountify__reviews--backgroundImage-wrapper">
+        <img
+          className="mountify__review--backgroundImage"
+          src={backGroundImage}
+          alt="Background image"
+          aria-hidden="true"
+        />
+      </div>
 
-      <h3 className="mountify__reviews-title">
-        <span className="facebookText facebookTxt-bg">Facebook</span>
-        Reviews
-      </h3>
+      <div className="mountify__reviews--content">
+        <h3 className="mountify__reviews-title">
+          <span className="facebookText facebookTxt-bg">Facebook</span>
+          Reviews
+        </h3>
 
-      <div className="mountify__reviews--cards">
-        {reviews.map((review, index) => (
-          <div key={index} className="mountify__reviews--review_card">
-            <h4 className="mountify__reviews--review-name">{review.name}</h4>
-            <div className="mountify__reviews--review-stars">
-              {review.stars}
+        <div className="mountify__reviews--cards">
+          {reviews.map((review, index) => (
+            <div key={index} className="mountify__reviews--review_card">
+              <h4 className="mountify__reviews--review-name">{review.name}</h4>
+              <div className="mountify__reviews--review-stars">
+                {review.stars}
+              </div>
+              <p className="mountify__reviews--review-comment">
+                {review.comment}
+              </p>
             </div>
-            <p className="mountify__reviews--review-comment">
-              {review.comment}
-            </p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
